ff95cbee564ed60a7f9f0e1b06d556fc32df1760
Thảo mai tắm <109426941+Thaomtam@users.noreply.github.com>
2025-06-04 19:11:56 +0700

    Update QSlockHook.kt

--- app/src/main/java/com/KTA/QSlock/hook/QSlockHook.kt
+++ app/src/main/java/com/KTA/QSlock/hook/QSlockHook.kt
@@ -4,42 +4,27 @@ import android.app.AndroidAppHelper
 import android.content.Context
 import android.util.Log
 import com.github.kyuubiran.ezxhelper.utils.*
-import de.robv.android.xposed.XposedHelpers
+import de.robv.android.xposed.XC_MethodHook
 
 object QSlockHook : BaseHook() {
     private const val TAG = "QSlock"
+
     private const val DISABLE2_NONE = 0
     private const val DISABLE2_QUICK_SETTINGS = 1
 
     override fun init() {
-        // Hook phương thức updateDisplays trong KeyguardDisplayManager
-        findClass("com.android.keyguard.KeyguardDisplayManager")?.findMethod {
+        findMethod("com.android.keyguard.KeyguardDisplayManager") {
             name == "updateDisplays"
-        }?.hookAfter {
-            Log.i(TAG, "show")
-
-            val context = AndroidAppHelper.currentApplication()
-            if (context == null) {
-                Log.e(TAG, "context is null")
-                return@hookAfter
-            }
-
+        }.hookBefore {
+            val context: Context = AndroidAppHelper.currentApplication().applicationContext
             val statusBarManager = context.getSystemService("statusbar")
-            if (statusBarManager == null) {
-                Log.e(TAG, "statusBarManager is null")
-                return@hookAfter
+            try {
+                val disable2Method = statusBarManager?.javaClass?.getMethod("disable2", Int::class.javaPrimitiveType)
+                disable2Method?.invoke(statusBarManager, DISABLE2_QUICK_SETTINGS)
+                Log.i(TAG, "Called disable2(DISABLE2_QUICK_SETTINGS)")
+            } catch (t: Throwable) {
+                Log.e(TAG, "disable2 failed", t)
             }
-
-            val isLockScreenActive = args.getOrNull(0)?.toString() == "true"
-            val disableFlag = if (isLockScreenActive) DISABLE2_QUICK_SETTINGS else DISABLE2_NONE
-
-            // Gọi phương thức disable2 của statusBarManager
-            XposedHelpers.callMethod(
-                statusBarManager,
-                "disable2",
-                arrayOf<Class<*>>(Integer::class.java),
-                disableFlag
-            )
         }
     }
 }
